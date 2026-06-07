6b1a32b25a2405c0eb5274730838960b3a3a5761
tkdeshan <tkdeshan1103@gmail.com>
2026-06-07 21:46:36 +0530

    ADD COMPONENT LIST

---
+++ assests/constant/components.js
@@ -0,0 +1,6 @@
+const COMPONENTS = [
+  {
+    name: "Arduino Uno",
+    image: "https://cdn.shopify.com/s/files/1/2719/6506/files/Arduino-Uno-R3-Clone-with-USB-Cable-3D-Printing-Canada-39008816.JPG?v=1726517065"
+  },
+];
