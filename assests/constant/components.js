aaf9aab45f2e515a52929ed2afc0cc015eda81c3
tkdeshan <tkdeshan1103@gmail.com>
2026-06-07 22:35:43 +0530

    add local images

--- assests/constant/components.js
+++ assests/constant/components.js
@@ -1,6 +1,10 @@
 const COMPONENTS = [
   {
     name: "Arduino Uno",
-    image: "https://cdn.shopify.com/s/files/1/2719/6506/files/Arduino-Uno-R3-Clone-with-USB-Cable-3D-Printing-Canada-39008816.JPG?v=1726517065"
+    image: "assests/img/components/arduino uno.png",
+  },
+{
+    name: "Ultrasonic Sensor",
+    image: "assests/img/components/ultrasonic sensor.png",
   },
 ];
